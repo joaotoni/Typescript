9971886e85844ed5006cb5ed97bd926993c35e10
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-06-11 12:46:48 -0300

    Começando os estudos em Typescript

---
+++ index.ts
@@ -0,0 +1,16 @@
+// {
+//     "postId": 1,
+//     "id": 1,
+//     "name": "id labore ex et quam laborum",
+//     "email": "Eliseo@gardner.biz",
+//     "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+//   },
+
+type Tipo = {
+    postId: number;
+    id: number;
+    nome: string;
+    email:string;
+    body: string;
+
+}
